8fa78fbb538575a4e80e0d2c18b2d7cbf9500d9c
marinakravchuk <marina.kravchuk.it@gmail.com>
2024-04-07 14:28:51 +0200

    First

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    // Or if using `src` directory:
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {},
+  plugins: [],
+};
+export default config;
